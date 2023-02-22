1d9ec6c9f2b9f2bfb7c1f7ccde3ade03f4e5d116
E-Ping Rau <epr26@cam.ac.uk>
2023-02-22 16:17:00 +0000

    deleted crop.r

--- crop.r
+++
@@ -1,8 +0,0 @@
-
-for (i in 1991:2021) {
-    dist = terra::rast(paste0("/maps/epr26/jrc_dist/jrc_dist_", i, ".tif"))
-    gf_contour = terra::vect("./Disturbance/gadm41_GUF_0.shp")
-    dist_cropped = terra::crop(dist, gf_contour)
-    terra::writeRaster(dist_cropped, paste0("/maps/epr26/jrc_dist/jrc_dist_cropped_", i, ".tif"), overwrite = T)
-    print(paste0(i, " done"))
-}
