9e44ef609c940617a7ba0ecfb26f72016a740a6c
E-Ping Rau <epr26@cam.ac.uk>
2023-02-22 16:27:23 +0000

    reprojects to local crs and polygonizes; should be temporary, polygonize part hasn't been worked into patch.py

--- reproj_polyg.sh
+++ reproj_polyg.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-for i in {1995..2021}
+#reproject from global to local CRS; subsequently integrated into patch.py
+for i in {1991..2021}
 do
   gdalwarp -s_srs EPSG:4326 -t_srs EPSG:32622 -r bilinear /maps/epr26/jrc_dist/jrc_dist_cropped_$i.tif /maps/epr26/jrc_dist/jrc_dist_prj_$i.tif
 done
 
-gdal_polygonize.py /maps/epr26/jrc_dist/jrc_dist_prj_1991.tif -f "ESRI Shapefile" jrc_dist_poly_1991_test.shp
+#polygonize; not yet integrated into patch.py
+for i in {1991..2021}
+do
+  gdal_polygonize.py /maps/epr26/jrc_dist/jrc_dist_prj_$i.tif -f "ESRI Shapefile" /maps/epr26/jrc_dist_shp/jrc_dist_poly_$i.shp
+done
